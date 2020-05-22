c34df1f2aa8a1e22d842387a12915220b8aeaef2
Ash Hellwig <ahellwig.dev@gmail.com>
2020-05-22 02:27:23 -0600

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ashhellwig-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ashhellwig_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns ashhellwig-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [ashhellwig-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
